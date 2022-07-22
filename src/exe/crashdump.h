c7cbead142b5452932faa647e5a6c6dfa96ec9c3
Pete x2 <109822278+ppalfajer@users.noreply.github.com>
2022-07-22 12:12:39 -0700

    Add files via upload

---
+++ src/exe/crashdump.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void CrashDumpInitialize();
+LONG CALLBACK CrashDumpVectorHandler(EXCEPTION_POINTERS* ExceptionInfo);
+void InvalidParameterHandler(const wchar_t* Expression, const wchar_t* Function, const wchar_t* File, unsigned int Line, uintptr_t Reserved);
+void TerminateHandler();
+void AbortHandler(int Signal);
